e3542e23f2ff3b8aa913d726cdafed7d9bb7e72e
Annette Poh <coding.net.p@gmail.com>
2021-12-27 20:06:05 +0800

    add device responsiveness to profile summary page

--- src/pages/ProfileSumPage.js
+++ src/pages/ProfileSumPage.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function ProfileSumPage(){
     return (
         <React.Fragment>
-            <div className='row mt-5'>
-                <div className='col-2'></div>
+            <div className='row mt-4 mt-md-5'>
+                <div className='d-none d-lg-block col-2'></div>
                 <div className='col'>
                     <div className="card-img-top profile-card">
                         {/* <img src={require('../images/profile/annette-profile.jpg').default} alt="profile"/> */}
@@ -13,19 +13,20 @@ export default function ProfileSumPage(){
                             <p className="card-text text-center">
                                 <h5>Entrepreneur</h5>
                                 <h5>Product Management</h5>
-                                <h5>Software Application Development</h5>
+                                <h5>Software Development</h5>
                             </p>
                         </div>
                     </div>
                 </div>
-                <div className='col'>
-                    <h2>Hi, I'm Annette</h2>
-                    <p>I am entreprennuer, product innovator and coder.</p>
-                    <p>
-                        I have strong passion about business, user experience and technology. Building innovative products and application that help streamline business, user-friendly product solutions, keep track on industry trends and emerging technologies. 
+                <hr className='d-block d-md-none h-line'></hr>
+                <div className='col sum-text'>
+                    <h2 className='mt-1 mt-lg-5'>Hi, I'm Annette</h2>
+                    <p className='intro-text'>I am entreprennuer, product innovator and software developer.</p>
+                    <p className='intro-text'>
+                        I have strong passion about business, user experience and technology. Building innovative products and applications that help streamline business, user-friendly product solutions, keep track of industry trends and emerging technologies. 
                     </p>
                 </div>
-                <div className='col-1'></div>
+                <div className='d-none d-lg-block col-1'></div>
             </div>
         </React.Fragment>
     )
