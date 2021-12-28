bf723800b57c319ea5bbcbba91ea49c9a79dcd4c
Annette Poh <coding.net.p@gmail.com>
2021-12-28 16:03:25 +0800

    add logic to display json data in experience page

--- src/pages/ExperiencePage.js
+++ src/pages/ExperiencePage.js
@@ -1,35 +1,42 @@
 import React from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faCircle } from '@fortawesome/free-solid-svg-icons'
+import jobExperiences from '../data/experiences.json'
 
 export default function ExperiencePage(){
     return (
         <React.Fragment>
             <h3 className='text-center mt-5 mb-5'>Experiences</h3>
             <div class="container px-4">
-                <div class="row gx-5">
-                    {/* <div class="col-1"></div> */}
-                    <div class="col-4 ms-5">
-                        <div class="p-3 text-end">
-                            <h5 className='mb-3'>Founder, Product Manager, Designer</h5>
-                            <h6>03/2021 - present</h6>
+                {jobExperiences.map(jobExperience => {
+                    return (
+                        <div class="row gx-5">
+                            <div class="col-4 ms-4">
+                                <div class="p-3 text-end">
+                                    <h5 className='mb-3'>{jobExperience.title}</h5>
+                                    <h6>{jobExperience.start} - {jobExperience.end}</h6>
+                                </div>
+                            </div>
+                            <div class="col-1">
+                                <FontAwesomeIcon icon={faCircle} className="circle-icon"/>
+                            </div>
+                            <div class="col">
+                                <div class="p-3 me-4">
+                                    <h5>{jobExperience.company} 
+                                        <span className='country'>({jobExperience.country})</span>
+                                    </h5>
+                                    <ul>
+                                        {jobExperience.responsibilities.map (responsibility => {
+                                            return (
+                                                <li>{responsibility}</li>
+                                            )
+                                        })} 
+                                    </ul> 
+                                </div>
+                            </div>
                         </div>
-                    </div>
-                    <div class="col-1">
-                        <FontAwesomeIcon icon={faCircle} className="circle-icon mt-4"/>
-                    </div>
-                    <div class="col">
-                        <div class="p-3">
-                            <h5>Sophisticated Me</h5>
-                            <ul>
-                                <li>Manage product lifecycle from ideation to launch and growth</li>
-                                <li>Pilot customer engagement for every prototypes and sample pieces (MVPs) of new product development to get market-fit products</li>
-                                <li>Successful in customer discovery, get early adopters and maintain customers loyalty</li>
-                            </ul> 
-                        </div>
-                    </div>
-                    <div class="col-1"></div>
-                </div>
+                    )
+                })}
                 <div className='vertical-line'></div>
             </div>
 
@@ -45,7 +52,7 @@ export default function ExperiencePage(){
 //     "country": "",
 //     "start": "",
 //     "end": "",
-//     "responsibility": [
+//     "responsibilities": [
 //         "",
 //         "",
 //         "",
