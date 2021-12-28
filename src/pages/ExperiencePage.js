c8135ae628555a4cc030ed52f62b9385715ecdb0
Annette Poh <coding.net.p@gmail.com>
2021-12-28 17:25:06 +0800

    add device responsiveness to experience page

--- src/pages/ExperiencePage.js
+++ src/pages/ExperiencePage.js
@@ -6,38 +6,39 @@ import jobExperiences from '../data/experiences.json'
 export default function ExperiencePage(){
     return (
         <React.Fragment>
-            <h3 className='text-center mt-5 mb-5'>Experiences</h3>
-            <div class="container px-4">
-                {jobExperiences.map(jobExperience => {
+            <h3 className='text-center mt-4 mt-md-5 mb-3 mb-md-4 mb-lg-5'>Experiences</h3>
+            <div className="container px-2 px-lg-4">
+                {jobExperiences.map((jobExperience, jobIndex) => {
                     return (
-                        <div class="row gx-5">
-                            <div class="col-4 ms-4">
-                                <div class="p-3 text-end">
+                        <div key={jobIndex} className="row gx-5">
+                            <div className="col-12 col-md-4 ms-0 ms-lg-4">
+                                <div className="p-0 p-lg-3 mt-3 mt-lg-0 job-title">
                                     <h5 className='mb-3'>{jobExperience.title}</h5>
-                                    <h6>{jobExperience.start} - {jobExperience.end}</h6>
+                                    <h6 className='duration'>{jobExperience.start} - {jobExperience.end}</h6>
                                 </div>
                             </div>
-                            <div class="col-1">
+                            <div className="d-none d-md-block col-1">
                                 <FontAwesomeIcon icon={faCircle} className="circle-icon"/>
                             </div>
-                            <div class="col">
-                                <div class="p-3 me-4">
+                            <div className="col">
+                                <div className="mt-3 mt-md-0 p-0 p-md-3 me-0 me-lg-4">
                                     <h5>{jobExperience.company} 
                                         <span className='country'>({jobExperience.country})</span>
                                     </h5>
                                     <ul>
-                                        {jobExperience.responsibilities.map (responsibility => {
+                                        {jobExperience.responsibilities.map ((responsibility, resIndex) => {
                                             return (
-                                                <li>{responsibility}</li>
+                                                <li key={resIndex}>{responsibility}</li>
                                             )
                                         })} 
                                     </ul> 
                                 </div>
                             </div>
+                            <hr className='d-block d-md-none experiences-hline'></hr>
                         </div>
                     )
                 })}
-                <div className='vertical-line'></div>
+                <div className='vertical-line experiences-hline'></div>
             </div>
 
         </React.Fragment>
